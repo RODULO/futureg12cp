8adcfdb1f5f84e21c08ef8d897900d9380b184ff
RODULO <rodu430@gmail.com>
2025-06-21 12:43:44 -0500

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+import 'package:futuresg12/pages/home_pages.dart';
+
+void main (){
+
+  runApp(MaterialApp(home: HomePages(), debugShowCheckedModeBanner: false));
+}
